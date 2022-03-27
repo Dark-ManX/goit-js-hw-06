59eae88de02b213f9cde5c6832dad38548335c20
Dark-ManX <sergiosis@ukr.net>
2022-03-27 11:32:08 +0300

    27.03.22 11:32

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const bodyRef = document.querySelector('body');
+const colorRef = document.querySelector('.color');
+const btnRef = document.querySelector('.change-color');
+
+btnRef.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  colorRef.textContent = `${getRandomHexColor()}`;
+  bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
+const refs = {
+  divControlRef: document.querySelector('#controls'),
+  inputRef: document.querySelector('input'),
+  createRef: document.querySelector('[data-create]'),
+  destroyRef: document.querySelector('[data-destroy]'),
+  divForCreatedEl: document.querySelector('#boxes'),
+}
+const { divControlRef,
+  inputRef,
+  createRef,
+  destroyRef,
+  divForCreatedEl,} = refs
+
+createRef.addEventListener('click', onCreateClick);
+destroyRef.addEventListener('click', onDestroyClick);
+
+function onCreateClick(event) {
+  if (inputRef.value <= 0) {
+    return null;
+  };
+  for (let i = 0; i < inputRef; i += 1) {
+    let size = size[0] = 30;
+    size[i] += 10;
+    divForCreatedEl.innerHTML = `<div width="${size[i]}px", height="${size[i]}px", background-color="${getRandomHexColor()}">`;
+  }
+};
+
+function onDestroyClick(event) {
+  divForCreatedEl.innerHTML = '';
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
